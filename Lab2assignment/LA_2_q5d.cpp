cbe6d778bd4f231c296fc026eb140c26148e4476
Nikhil Khosla <nikhilkh06@gmail.com>
2025-08-18 17:41:06 +0530

    q5partd

---
+++ Lab2assignment/LA_2_q5d.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main() {
+    
+    int n;
+    cout<<"enter size of matrix"<<endl;
+    cin>>n;
+    int arr[n][n];
+    cout<<"enter the elements in matrix"<<endl;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i>j)
+            cout<<"0";
+            else cout<<arr[i][j];
+        }
+    cout<<endl;
+    }
+    return 0;
+}
