3e7a9768005d5cb15a2d97b2df5673f2e933f661
Nikhil Khosla <nikhilkh06@gmail.com>
2025-08-24 00:48:02 +0530

    q7

---
+++ Lab2assignment/LA_2_q7.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int countInversions(int arr[], int n) {
+    int count = 0;
+    for(int i=0; i<n-1; i++) {
+        for(int j=i+1; j<n; j++) {
+            if(arr[i] > arr[j]) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+int main() {
+    int n;
+    cout << "enter size of array: ";
+    cin >> n;
+    int arr[n];
+    cout << "enter elements: ";
+    for(int i=0; i<n; i++) {
+        cin >> arr[i];
+    }
+    cout << "no of inversions: " << countInversions(arr, n) << endl;
+    return 0;
+}
